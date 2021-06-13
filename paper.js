88c4be6c581885d7fbfbeb1e33ddf78e9c572a44
NoelVargheseChandy <81355575+NoelVargheseChandy@users.noreply.github.com>
2021-06-13 19:24:55 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,32 @@
+class Paper
+{
+	constructor(x,y,w,h)
+	{
+		var options={
+			isStatic:false
+            restitution:0.3,
+            friction:0,
+            density:1.2		
+			}
+		this.x=x;
+		this.y=y;
+		this.w=w
+		this.h=h
+		this.body=Bodies.rectangle(x, y, w, h , options);
+ 		World.add(world, this.body);
+        this.image=loadImage("paper.js");
+	}
+	display()
+	{
+			
+					
+
+			push()
+			
+			rectMode(CENTER)
+			//strokeWeight(4);
+			fill(128,128,128)
+			rect(0,0,this.w, this.h);
+			pop()
+			
+    }
